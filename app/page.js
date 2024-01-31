7edb0d996e20ec9e660ddcfe0cd08cad7adbd1ad
Martin Fojtek <martfojt@gmail.com>
2024-01-31 20:36:35 +0100

    Add styles on image hover

--- app/page.js
+++ app/page.js
@@ -2,16 +2,22 @@ import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <body className={styles.backgroundImage}>
-      <div>
-        <div className={styles.navbar}>Navbar</div>
-        <div className={styles.mainBox}>
-          <div className={`${styles.pictures} ${styles.picture1}`}> </div>
-          <div className={`${styles.pictures} ${styles.picture2}`}> </div>
-          <div className={`${styles.pictures} ${styles.picture3}`}> </div>
-          <div className={`${styles.pictures} ${styles.picture4}`}> </div>
+    <div className={styles.backgroundImage}>
+      <div className={styles.navbar}>Navbar</div>
+      <div className={styles.mainBox}>
+        <div className={`${styles.pictures} ${styles.picture1}`}>
+          <span className={styles.hoverText}>Text for Picture 1</span>
+        </div>
+        <div className={`${styles.pictures} ${styles.picture2}`}>
+          <span className={styles.hoverText}>Text for Picture 1</span>
+        </div>
+        <div className={`${styles.pictures} ${styles.picture3}`}>
+          <span className={styles.hoverText}>Text for Picture 1</span>
+        </div>
+        <div className={`${styles.pictures} ${styles.picture4}`}>
+          <span className={styles.hoverText}>Text for Picture 1</span>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
