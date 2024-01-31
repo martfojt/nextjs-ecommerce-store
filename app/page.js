eb97d08c3f3c805fcff80a6305a46bdd7d9b77c1
Martin Fojtek <martfojt@gmail.com>
2024-01-31 19:20:01 +0100

    Add basic main page layout

--- app/page.js
+++ app/page.js
@@ -1,6 +1,18 @@
-import Image from 'next/image';
+import global from './global.css';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <body>
+      <div className={styles.backgroundImage}>
+        <div className={styles.navbar}>Navbar</div>
+        <div className={styles.mainBox}>
+          <div className={styles.pictures}>Picture 1</div>
+          <div className={styles.pictures}>Picture 2</div>
+          <div className={styles.pictures}>Picture 3</div>
+          <div className={styles.pictures}>Picture 4</div>
+        </div>
+      </div>
+    </body>
+  );
 }
