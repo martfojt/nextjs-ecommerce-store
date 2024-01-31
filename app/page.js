9df07ff635a8b154f51233926318a692d9e75a87
Martin Fojtek <martfojt@gmail.com>
2024-01-31 19:51:16 +0100

    Basic main page layout with placeholder images

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
-import global from './global.css';
-import styles from './page.module.css';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <body>
-      <div className={styles.backgroundImage}>
+    <body className={styles.backgroundImage}>
+      <div>
         <div className={styles.navbar}>Navbar</div>
         <div className={styles.mainBox}>
-          <div className={styles.pictures}>Picture 1</div>
-          <div className={styles.pictures}>Picture 2</div>
-          <div className={styles.pictures}>Picture 3</div>
-          <div className={styles.pictures}>Picture 4</div>
+          <div className={`${styles.pictures} ${styles.picture1}`}> </div>
+          <div className={`${styles.pictures} ${styles.picture2}`}> </div>
+          <div className={`${styles.pictures} ${styles.picture3}`}> </div>
+          <div className={`${styles.pictures} ${styles.picture4}`}> </div>
         </div>
       </div>
     </body>
