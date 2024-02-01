edec0b25253db31878c64d63cd6e04a6d26659c9
Martin Fojtek <martfojt@gmail.com>
2024-02-01 14:56:39 +0100

    Add header to all pages

--- app/middle-earth/page.js
+++ app/middle-earth/page.js
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import styles from '../page.module.scss';
+
 export const metadata = {
   title: {
     default: 'Home Page',
@@ -8,9 +11,16 @@ export const metadata = {
 
 export default function MiddleEarth() {
   return (
-    <div>
-      <h1>Welcome to Middle-Earth</h1>
-      {}
-    </div>
+    <header className={styles.navbar}>
+      <div className={styles.test}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <img
+          src="./shopping-basket.png"
+          alt="Shopping cart"
+          className={styles.shoppingCart}
+        />
+      </div>
+    </header>
   );
 }
