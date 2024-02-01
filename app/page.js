b97808a73ce5e96220d747901ca2eadbed216d90
Martin Fojtek <martfojt@gmail.com>
2024-02-01 13:55:02 +0100

    Add links to all product pages

--- app/page.js
+++ app/page.js
@@ -14,24 +14,30 @@ export default function Home() {
             </span>
           </div>
         </Link>
-        <div className={styles.pictures}>
-          <div className={`${styles.background} ${styles.picture2}`}> </div>
-          <span className={styles.hoverText}>
-            <h1>Travel to Cadia</h1>
-          </span>
-        </div>
-        <div className={styles.pictures}>
-          <div className={`${styles.background} ${styles.picture3}`}> </div>
-          <span className={styles.hoverText}>
-            <h1>Experience Hogwarts</h1>
-          </span>
-        </div>
-        <div className={styles.pictures}>
-          <div className={`${styles.background} ${styles.picture4}`}> </div>
-          <span className={styles.hoverText}>
-            <h1>Journey to Azeroth</h1>
-          </span>
-        </div>
+        <Link href="/cadia">
+          <div className={styles.pictures}>
+            <div className={`${styles.background} ${styles.picture2}`}> </div>
+            <span className={styles.hoverText}>
+              <h1>Travel to Cadia</h1>
+            </span>
+          </div>
+        </Link>
+        <Link href="/hogwarts">
+          <div className={styles.pictures}>
+            <div className={`${styles.background} ${styles.picture3}`}> </div>
+            <span className={styles.hoverText}>
+              <h1>Experience Hogwarts</h1>
+            </span>
+          </div>
+        </Link>
+        <Link href="/azeroth">
+          <div className={styles.pictures}>
+            <div className={`${styles.background} ${styles.picture4}`}> </div>
+            <span className={styles.hoverText}>
+              <h1>Journey to Azeroth</h1>
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
