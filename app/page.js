510144227d2927f07e9bfb88d9980b8b93ec8ca0
Martin Fojtek <martfojt@gmail.com>
2024-02-01 12:08:38 +0100

    Edit styles for divs with images

--- app/page.js
+++ app/page.js
@@ -6,16 +6,16 @@ export default function Home() {
       <header className={styles.navbar}>Header</header>
       <div className={styles.mainBox}>
         <div className={`${styles.pictures} ${styles.picture1}`}>
-          <span className={styles.hoverText}>Text for Picture 1</span>
+          <span className={styles.hoverText}>Explore Middle-Earth</span>
         </div>
         <div className={`${styles.pictures} ${styles.picture2}`}>
-          <span className={styles.hoverText}>Text for Picture 1</span>
+          <span className={styles.hoverText}>Travel to Cadia</span>
         </div>
         <div className={`${styles.pictures} ${styles.picture3}`}>
-          <span className={styles.hoverText}>Text for Picture 1</span>
+          <span className={styles.hoverText}>Experience Hogwarts</span>
         </div>
         <div className={`${styles.pictures} ${styles.picture4}`}>
-          <span className={styles.hoverText}>Text for Picture 1</span>
+          <span className={styles.hoverText}>Journey to Azeroth</span>
         </div>
       </div>
     </div>
