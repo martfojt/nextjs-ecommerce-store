56314960ef8ed4c34f1a2914668ce13128c4026f
Martin Fojtek <martfojt@gmail.com>
2024-02-01 14:34:21 +0100

    Edit header

--- app/page.js
+++ app/page.js
@@ -4,7 +4,17 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div>
-      <header className={styles.navbar}>Header</header>
+      <header className={styles.navbar}>
+        <div className={styles.test}>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <img
+            src="./shopping-basket.png"
+            alt="Shopping cart"
+            className={styles.shoppingCart}
+          />
+        </div>
+      </header>
       <div className={styles.mainBox}>
         <Link href="/middle-earth">
           <div className={styles.pictures}>
