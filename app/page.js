88e2617aa6ee60703c1e8ce147f0185dc32f9f94
Martin Fojtek <martfojt@gmail.com>
2024-02-01 11:51:00 +0100

    Edit navbar to match the assignment

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div>
-      <div className={styles.navbar}>Navbar</div>
+      <header className={styles.navbar}>Header</header>
       <div className={styles.mainBox}>
         <div className={`${styles.pictures} ${styles.picture1}`}>
           <span className={styles.hoverText}>Text for Picture 1</span>
