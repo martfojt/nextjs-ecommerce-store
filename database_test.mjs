9d61d3b742e713ff623099544d109606bdef7026
Martin Fojtek <martfojt@gmail.com>
2024-02-04 19:04:20 +0100

    Add dotenv example file

--- database_test.mjs
+++ database_test.mjs
@@ -1,7 +1,24 @@
+import { readFileSync } from 'node:fs';
 import dotenv from 'dotenv';
 import postgres from 'postgres';
 
-dotenv.config();
+function setEnvironmentVariables() {
+  dotenv.config();
+
+  const unconfiguredEnvVars = Object.keys(
+    dotenv.parse(readFileSync('./.env.example')),
+  ).filter((exampleKey) => !process.env[exampleKey]);
+
+  if (unconfiguredEnvVars.length > 0) {
+    throw new Error(
+      `.env.example environment ${
+        unconfiguredEnvVars.length > 1 ? 'variables' : 'variable'
+      } ${unconfiguredEnvVars.join(', ')} not configured in .env file`,
+    );
+  }
+}
+
+setEnvironmentVariables();
 
 const sql = postgres();
 
